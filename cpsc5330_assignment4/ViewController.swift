bc5aeac90fda0b25a61259f86361898d25bf14c6
dandye <danrdye@gmail.com>
2025-02-09 10:53:03 -0500

    Added timer and music

--- cpsc5330_assignment4/ViewController.swift
+++ cpsc5330_assignment4/ViewController.swift
@@ -6,33 +6,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var clockLabel: UILabel!
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var remainingLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
     
     var currentDateTime = Date()
     let clockFormat = "EEE, dd MMM yyyy HH:mm:ss"
     var remainingTime = 0.0
     let timeFormat = "HH:mm:ss"
     var countDownTimer = Timer()
+    var timerEnded = false
+    var audioPlayer : AVAudioPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        startButton.setTitle("Start Timer", for: .normal)
+        remainingLabel.text = "Time remaining --:--:--"
         let timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(updateClock), userInfo: nil, repeats: true)
-        // Do any additional setup after loading the view.
-        
-        
-        
     }
     
     @IBAction func buttonClick(_ sender: UIButton) {
-        countDownTimer.invalidate()
-        remainingTime = datePicker.countDownDuration
-        countDownTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateRemaingTime), userInfo: nil, repeats: true)
-        
+        if (!timerEnded) {
+            countDownTimer.invalidate()
+            remainingTime = datePicker.countDownDuration
+            countDownTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateRemaingTime), userInfo: nil, repeats: true)
+        }
+        else {
+            startButton.setTitle("Start Timer", for: .normal)
+            audioPlayer!.stop()
+            timerEnded = false
+        }
+
     }
     
     @objc func updateClock() {
@@ -44,18 +53,35 @@ class ViewController: UIViewController {
     
     @objc func updateRemaingTime() {
         if (remainingTime > 0) {
-            remainingLabel.text = "Time Remaining \(remainingTime)"
+            remainingLabel.text = "Time Remaining \(timeFormatter(remainingTime))"
             remainingTime -= 1
         }
         else {
             countDownTimer.invalidate()
-            remainingLabel.text = "Time Remaining \(remainingTime)"
-            print("\(remainingTime)")
+            remainingLabel.text = "Time Remaining \(timeFormatter(remainingTime))"
+            startButton.setTitle("Stop Music", for: .normal)
+            timerEnded = true
+            playMusic()
         }
     }
     
-
-
-
+    func timeFormatter(_ time: TimeInterval) -> String {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.hour, .minute, .second]
+        formatter.zeroFormattingBehavior = [.pad]
+        return formatter.string(from: time) ?? "00:00:00"
+    }
+    
+    func playMusic() {
+        let path = Bundle.main.path(forResource: "song", ofType: "mp3")
+        let url = URL(fileURLWithPath: path!)
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer!.play()
+        } catch {
+            print("Song.mp3 not found")
+        }
+        
+    }
 }
 
